4cacfcd45379a5af625f88051cac813e6bc65315
rciochin <ciochina7@gmail.com>
2023-05-05 17:01:53 +0300

    structure done

---
+++ app/index.js
@@ -0,0 +1,13 @@
+import {View, Text, View, ScrollView, SafeAreaView} from 'react-native';
+import {useState} from 'react';
+import {Stack, useRouter} from 'expo-router';
+
+const Home = () => {
+    return (
+        <View>
+        <Text>Home</Text>
+        </View>
+    );
+}
+
+export default Home;
