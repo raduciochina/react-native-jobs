0beffc12ad3083cd500558489c7bdbec4053f530
rciochin <ciochina7@gmail.com>
2023-05-16 16:46:57 +0300

    work

--- app/index.js
+++ app/index.js
@@ -1,45 +1,59 @@
-import {View, Text, ScrollView, SafeAreaView} from 'react-native';
-import {useState} from 'react';
-import {Stack, useRouter} from 'expo-router';
-import {COLORS, icons, SIZES, images} from '../constants';
-import {Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome} from '../components';
+import { View, Text, ScrollView, SafeAreaView } from "react-native";
+import { useState } from "react";
+import { Stack, useRouter } from "expo-router";
+import { COLORS, icons, SIZES, images } from "../constants";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
 
 const Home = () => {
-    const router = useRouter();
+  const router = useRouter();
 
-    return (
-        <SafeAreaView style={{
-            flex: 1,
-            backgroundColor: COLORS.lightWhite
-        }}>
-        <Stack.Screen
+  return (
+    <SafeAreaView
+      style={{
+        flex: 1,
+        backgroundColor: COLORS.white,
+      }}
+    >
+      <Stack.Screen
         options={{
-            headerStyle: {backgroundColor: COLORS.lightWhite},
-            headerShadowVisible: false,
-            headerLeft: () => (
-                <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%"/>
-            ),
-            headerRight: () => (
-                <ScreenHeaderBtn iconUrl={images.profile} dimension="60%"/>
-            ),
-            headerTitle: "",
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn
+              iconUrl={icons.menu}
+              onPress={() => {}}
+              dimension="60%"
+            />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn
+              iconUrl={images.profile}
+              onPress={() => {}}
+              dimension="100%"
+            />
+          ),
+          headerTitle: "",
         }}
-        />
-
-        <ScrollView showsVerticalScrollIndicator={false}>
-            <View style={
-                {
-                    flex: 1,
-                    padding: SIZES.medium,
-                }
-            }>
-                <Welcome/>
-                <Popularjobs/>
-                <Nearbyjobs/>
-            </View>
-        </ScrollView>
-        </SafeAreaView>
-    );
-}
+      />
+      <ScrollView showsHorizontalScrollIndicator={false}>
+        <View
+          style={{
+            flex: 1,
+            padding: SIZES.medium,
+          }}
+        >
+          <Welcome />
+          <Popularjobs />
+          <Nearbyjobs />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
 export default Home;
--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,12 +1,47 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, {useState} from 'react'
+import { View, Text, TextInput, TouchableOpacity, Image, FlatList } from 'react-native'
+import { useRouter } from 'expo-router'
 
 import styles from './welcome.style'
+import { icons, SIZES } from '../../../constants'
+
+const jobsTypes = ["Full-time" , "Part-time" , "Freelance" , "Internship"]
 
 const Welcome = () => {
+  const router = useRouter()
+  const [activeJobType, setActiveJobType] = useState(jobsTypes[0])
   return (
     <View>
-      <Text>Welcome</Text>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Radu</Text>
+        <Text style={styles.welcomeMessage}>Find your perfect job!</Text>
+
+      </View>
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput
+          style={styles.searchInput}
+          placeholder="Search for jobs"
+          value=''
+          onChange={()=>{}}
+          />
+        </View>
+        <TouchableOpacity style={styles.searchBtn} onPress={()=>{}}>
+          <Image source={icons.search} style={styles.searchBtnImage} resizeMode='contain'/>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.tabsContainer}>
+        <FlatList data={jobsTypes} renderItem={
+          ({item}) => (
+            <TouchableOpacity style={styles.tab(activeJobType, item)} onPress={()=>{
+              setActiveJobType(item)
+              router.push(`/search/${item}`)
+            }}>
+            <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
+          </TouchableOpacity>
+          )
+        }/>
+      </View>
     </View>
   )
 }
