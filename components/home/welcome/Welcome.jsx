71e55ce9230da6d28038a9e22c19ae5a8ddfe643
rciochin <ciochina7@gmail.com>
2023-05-16 17:31:58 +0300

    problem with useFetch, dont know why

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -40,7 +40,11 @@ const Welcome = () => {
             <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
           </TouchableOpacity>
           )
-        }/>
+        }
+        keyExtractor={item => item}
+        contentContainerStyle={{columnGap: SIZES.small}}
+        horizontal
+        />
       </View>
     </View>
   )
---
+++ hook/useFetch.js
@@ -0,0 +1,44 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useFetch = (endpoint, query) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const options = {
+    method: "GET",
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+    headers: {
+      "X-RapidAPI-Key": "f0c46a0f3dmsh1657f0bfd2e0f00p1865b6jsnb92a3ee40ec2",
+      "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
+    },
+    params: { ...query },
+  };
+  const fetchData = async () => {
+    setIsLoading(true);
+    try {
+      const response = await axios.request(options);
+      setData(response.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error);
+      setIsLoading(false);
+      alert(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const refetch = () => {
+    setIsLoading(true);
+    fetchData();
+  };
+
+  return { data, isLoading, error, refetch };
+};
+
+export default useFetch;
